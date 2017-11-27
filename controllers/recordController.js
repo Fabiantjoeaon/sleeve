b9096f0c69626fe3a9892e99c0ee2e80820606f0
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2017-11-27 15:11:13 +0100

    Basic record controller

---
+++ controllers/recordController.js
@@ -0,0 +1,49 @@
+const Record = require('../models/Record');
+
+/**
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {}
+ */
+const index = async (req, res) => {};
+
+/**
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {}
+ */
+const create = async (req, res) => {};
+
+/**
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {}
+ */
+const show = async (req, res) => {};
+
+/**
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {}
+ */
+const edit = async (req, res) => {};
+
+/**
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {}
+ */
+const destroy = async (req, res) => {};
+
+module.exports = {
+    index,
+    create,
+    show,
+    edit,
+    destroy
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 const authController = require('../controllers/authController');
+const recordController = require('../controllers/recordController');
 const {
     ensureLoggedIn,
     ensureLoggedOut
 } = require('../middleware/authMiddleware');
 
 module.exports = app => {
-    // app.get('/auth/logout', ensureLoggedIn, authController.logout);
+    app.options('/*', (req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET,PUT,POST,DELETE,OPTIONS'
+        );
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization, Content-Length, X-Requested-With'
+        );
+        res.send(200);
+    });
+
     app.post('/auth/register', ensureLoggedOut, authController.register);
     app.post('/auth/login', ensureLoggedOut, authController.login);
+
+    app.get('/records', recordController.index);
+    app.post('/records', recordController.create);
+    app.get('/records/:id', recordController.show);
+    app.put('/records/:id', recordController.edit);
+    app.delete('/records/:id/edit', recordController.destroy);
 
     app.get('*', (req, res) =>
         res.status(404).send({
