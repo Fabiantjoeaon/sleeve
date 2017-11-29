357c4eef7bd67af733596bcf73d4fc3f3d2fc4af
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2017-11-29 14:15:46 +0100

    no message

--- models/Record.js
+++ models/Record.js
@@ -3,49 +3,47 @@ const validate = require('validate.js');
 
 const { Schema } = mongoose;
 
-const Record = new Schema(
-    {
-        name: {
-            type: String,
-            required: 'You must supply a name'
-        },
-        artist: {
-            type: String,
-            required: 'You must supply an artist'
-        },
-        description: {
-            type: String,
-            required: 'You must supply an description'
-        },
-        tracks: [
-            {
-                type: String,
-                required: 'You must supply a name for a track',
-                trackList: [
-                    {
-                        side: {
-                            type: String,
-                            required: 'You must supply a side for a track'
-                        },
-                        name: {
-                            type: String,
-                            required: 'You must supply a side for a track'
-                        }
-                    }
-                ]
-            }
-        ],
-        genre: {
-            type: String
-        }
+const Record = new Schema({
+    name: {
+        type: String,
+        required: 'You must supply a name'
     },
-    {
-        timestamps: {
-            createdAt: 'createdAt',
-            updatedAt: 'updatedAt'
-        }
+    artist: {
+        type: String,
+        required: 'You must supply an artist'
+    },
+    description: {
+        type: String,
+        required: 'You must supply an description'
     }
-);
+    //     tracks: [
+    //         {
+    //             type: String,
+    //             required: 'You must supply a name for a track',
+    //             trackList: [
+    //                 {
+    //                     side: {
+    //                         type: String,
+    //                         required: 'You must supply a side for a track'
+    //                     },
+    //                     name: {
+    //                         type: String,
+    //                         required: 'You must supply a side for a track'
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     ],
+    //     genre: {
+    //         type: String
+    //     }
+    // },
+    // {
+    //     timestamps: {
+    //         createdAt: 'createdAt',
+    //         updatedAt: 'updatedAt'
+    //     }
+});
 
 Record.pre('find', function(next) {
     this.bla = 'efuhef';
