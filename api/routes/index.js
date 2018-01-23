925d518358b7e0fa13ce28ec7a8638fe606fa818
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-01-23 17:51:57 +0100

    routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -23,17 +23,14 @@ const detailHeaderMiddleware = (req, res, next) => {
 
 router.options('/records', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.header('Allow', 'GET,POST,OPTIONS');
+
+    // res.header('Allow', 'GET,POST,OPTIONS');
     return res.sendStatus(200);
 });
 
 router.options('/records/:id', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT,GET, DELETE, OPTIONS');
-    res.header('Allow', 'PUT,GET, DELETE, OPTIONS');
+    // res.header('Allow', 'PUT,GET, DELETE, OPTIONS');
     return res.sendStatus(200);
 });
 
